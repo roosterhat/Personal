460a8e8128381f1cfb738a39f66ca91b31fd183d
Erik ostlind <erik.ostlind@att.net>
2024-05-03 12:38:47 -0500

    moved remaining command line configs to config file

--- AC-Control/Server/gunicorn.conf.py
+++ AC-Control/Server/gunicorn.conf.py
@@ -1,4 +1,7 @@
 bind = "0.0.0.0:3001"
+keyfile = "./certs/ACServer.key"
+certfile = "./certs/ACServer.crt"  
+wsgi_app = "Server:appStart()"
 
 def worker_exit(server, worker):
     print("worker_exit")
