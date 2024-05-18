64391a72c4f2567b3642b7a8d07f33020d950768
Erik ostlind <erik.ostlind@att.net>
2024-05-18 16:47:06 -0500

    Delete Racket directory

--- Racket/Program Design 2/gcd.rkt
+++
@@ -1,5 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname gcd) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
-(define (gcdb a b)(cond[(= (remainder a b)0)b]
-                      [else (gcdb b(- a(*(quotient a b)b)))]))
