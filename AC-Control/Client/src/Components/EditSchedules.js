c346c99ef9203c9aac9b0f56d0eef1326b0f7c3c
Erik ostlind <erik.ostlind@att.net>
2024-06-10 19:54:15 -0500

    updated schedule editor to include state name in config

--- AC-Control/Client/src/Components/EditSchedules.js
+++ AC-Control/Client/src/Components/EditSchedules.js
@@ -114,10 +114,12 @@ class Schedules extends React.Component {
             schedule.state.states.splice(index, 1)
         }
         if(id){
+            var state = group.states.find(x => x.id = id)
             schedule.state.states.push({
                 "id": id,
                 "groupId": group.id,
-                "active": true
+                "active": true,
+                "name": state.name
             })
         }
     }
